3bc001737d5fdf89b6ce1efa45091dfb3b56349b
Jesse White <jesse.white6@gmail.com>
2021-03-24 13:57:17 -0700

    complete addition functionalilty.
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
+
+
+function add(number1, number2) {
+    return number1 + number2;
+};
+
+function subtract(number1, number2) {
+    return number1 - number2;
+};
+
+function multiply(number1, number2) {
+    return number1 * number2;
+};
+
+function divide(number1, number2) {
+    return number1 / number2;
+};
+
+// Everything below this line is user interface logic:
 $(document).ready(function () {
+    $("form#add").submit(function (event) {
+        event.preventDefault();
 
-    function add(number1, number2) {
-        return number1 + number2;
-    };
-
-    function subtract(number1, number2) {
-        return number1 - number2;
-    };
-
-    function multiply(number1, number2) {
-        return number1 * number2;
-    };
-
-    function divide(number1, number2) {
-        return number1 / number2;
-    };
-
-    // Everything below this line is user interface logic:
-
-    const number1 = parseInt($("#add1").val());
-    const number2 = parseInt($("#add2").val());
-    const result = add(number1, number2);
-    console.log(result)
-    alert(result);
+        const number1 = parseInt($("#add1").val());
+        const number2 = parseInt($("#add2").val());
+        const result = add(number1, number2);
+        $("#output").text(result);
+    })
 })
