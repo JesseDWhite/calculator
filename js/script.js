93f8400fd02337c51605e852d88241edd52553ed
Jesse White <jesse.white6@gmail.com>
2021-03-24 14:12:27 -0700

    add three more arithmatic functions to the page.
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// Business logic
 
 function add(number1, number2) {
     return number1 + number2;
@@ -17,6 +17,7 @@ function divide(number1, number2) {
 };
 
 // Everything below this line is user interface logic:
+
 $(document).ready(function () {
     $("form#add").submit(function (event) {
         event.preventDefault();
@@ -24,6 +25,33 @@ $(document).ready(function () {
         const number1 = parseInt($("#add1").val());
         const number2 = parseInt($("#add2").val());
         const result = add(number1, number2);
-        $("#output").text(result);
+        $("#outputAdd").text(result);
     })
+
+    $("form#subtract").submit(function (event) {
+        event.preventDefault();
+
+        const number1 = parseInt($("#sub1").val());
+        const number2 = parseInt($("#sub2").val());
+        const result = subtract(number1, number2);
+        $("#outputSub").text(result);
+    });
+
+    $("form#multiply").submit(function (event) {
+        event.preventDefault();
+
+        const number1 = parseInt($("#mul1").val());
+        const number2 = parseInt($("#mul2").val());
+        const result = multiply(number1, number2);
+        $("#outputMul").text(result);
+    });
+
+    $("form#divide").submit(function (event) {
+        event.preventDefault();
+
+        const number1 = parseInt($("#div1").val());
+        const number2 = parseInt($("#div2").val());
+        const result = divide(number1, number2);
+        $("#outputDiv").text(result);
+    });
 })
