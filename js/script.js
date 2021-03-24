52ab5db2b09bd5f3c1d3b2c454b7eb166142ef6a
Jesse White <jesse.white6@gmail.com>
2021-03-24 13:31:51 -0700

    initial commit and adding co author.
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,53 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = alert(add(number1, number2));
+
+alert(result);
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 + " - " + number2 + " = " + (number1 - number2);
+}
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const difference = subtract(number1, number2);
+
+alert(difference);
+
+function multiply(number3, number4) {
+    return number3 + " * " + number4 + " = " + number3 * number4;
+}
+
+const number3 = parseInt(prompt("Enter a number:"));
+const number4 = parseInt(prompt("Enter another number:"));
+const product = multiply(number3, number4);
+
+alert(product);
+
+const result = add(number1, number2);
+
+function tempConversion(temperature) {
+    let fahTemp = ((9 / 5) * temperature) + 32;
+    let celTemp = ((9 / 5) * temperature) - 32;
+    alert(temperature + "  in fahrenheit: " + fahTemp);
+    alert(temperature + " in celsius: " + celTemp);
+}
+
+tempConversion(45);
+
+function volConversion(volume) {
+    let liter = volume / 3.785;
+    let gallon = volume * 3.785;
+    alert(volume + " liters in gallons: " + liter);
+    alert(volume + " gallons in liters: " + gallon);
+}
+
+volConversion(13);
